20243812da7dee2d5ae378c5dc262f63046a88fb
Donbabahmcts <aalasisi@outlook.com>
2025-12-07 17:21:04 +0000

    fix: update Jest configuration to enhance TypeScript support

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,17 @@ module.exports = {
   testRegex: '(/src/test/.*|\\.(test|spec))\\.(ts|js)$',
   moduleFileExtensions: ['ts', 'js', 'json'],
   testEnvironment: 'node',
+  modulePaths: ['<rootDir>/src'],
   transform: {
-    '^.+\\.ts?$': 'ts-jest',
+    '^.+\\.ts?$': ['ts-jest', {
+      tsconfig: {
+        esModuleInterop: true,
+        allowSyntheticDefaultImports: true,
+        moduleResolution: 'node',
+        types: ['jest', 'node'],
+        skipLibCheck: true,
+        isolatedModules: true,
+      },
+    }],
   },
 };
